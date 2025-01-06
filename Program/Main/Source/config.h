1f1561a5049c84c057d3e59fa1adfadac72c07ba
cw-20017497 <insik.choi@coway.com>
2025-01-06 18:01:42 +0900

    1. LED 밝기 테스트 기능 off

--- Program/Main/Source/config.h
+++ Program/Main/Source/config.h
@@ -4,8 +4,8 @@
 
 /* 통신 송수신 모니터링 기능 설정/해제 */
 #define DEBUG_COMM     1           /* 0: 해제, 1:설정 */
-#define CONFIG_TEST_LED         1
-#define CONFIG_TEST_LED_KEY     1
+#define CONFIG_TEST_LED         0
+#define CONFIG_TEST_LED_KEY     0
 
 /* 버전 */
 #define VERSION         1
